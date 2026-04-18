50be79bf8b698e96f6de3dc0d5115da13fecc086
iflex-lakshya-mishra <guptamartstationary@users.noreply.github.com>
2026-04-19 02:35:53 +0530

    fix: remove StrictMode to fix auth lock issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import { ThemeProvider } from './context/ThemeContext.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
 );
